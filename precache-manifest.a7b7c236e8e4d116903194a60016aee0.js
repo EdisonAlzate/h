4c18f6fd110f7edee7ccdeb65bd6d35967cda7fa
Edison Alzate <edisonalzate11@gmail.com>
2019-05-30 10:09:01 -0500

    Updates

---
+++ precache-manifest.a7b7c236e8e4d116903194a60016aee0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "869d71ffb4675068cfc5",
+    "url": "/h/static/css/main.4019af97.chunk.css"
+  },
+  {
+    "revision": "869d71ffb4675068cfc5",
+    "url": "/h/static/js/main.869d71ff.chunk.js"
+  },
+  {
+    "revision": "44e0c4016d6d324c1502",
+    "url": "/h/static/js/runtime~main.44e0c401.js"
+  },
+  {
+    "revision": "be5872d61d2ccc400acb",
+    "url": "/h/static/css/2.9eeac11d.chunk.css"
+  },
+  {
+    "revision": "be5872d61d2ccc400acb",
+    "url": "/h/static/js/2.be5872d6.chunk.js"
+  },
+  {
+    "revision": "d67cdb62851fe3f10f190d065048603c",
+    "url": "/h/static/media/webDeveloper.d67cdb62.jpg"
+  },
+  {
+    "revision": "06b7f4eed11a7d8f03d4e44605d4be40",
+    "url": "/h/static/media/programador.06b7f4ee.jpg"
+  },
+  {
+    "revision": "23bee0402b67daf062a993b0c8336efc",
+    "url": "/h/static/media/react.23bee040.png"
+  },
+  {
+    "revision": "0abcdfcd1e323649a0338a204fe8b853",
+    "url": "/h/static/media/stars.0abcdfcd.svg"
+  },
+  {
+    "revision": "ad0797b103ba8a78f0ac8a049afe9def",
+    "url": "/h/index.html"
+  }
+];
