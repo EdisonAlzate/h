e0b352ec04d6862f6e6ee1f9ca27f464368edc0c
Edison Alzate <edisonalzate11@gmail.com>
2019-05-30 11:01:54 -0500

    Updates

---
+++ precache-manifest.188fc869135f97656023f0568e12486d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e5e557d50920ead24f5",
+    "url": "/h/static/css/main.9f7f34fb.chunk.css"
+  },
+  {
+    "revision": "0e5e557d50920ead24f5",
+    "url": "/h/static/js/main.0e5e557d.chunk.js"
+  },
+  {
+    "revision": "44e0c4016d6d324c1502",
+    "url": "/h/static/js/runtime~main.44e0c401.js"
+  },
+  {
+    "revision": "4ea57a885486daf9c68c",
+    "url": "/h/static/css/2.9eeac11d.chunk.css"
+  },
+  {
+    "revision": "4ea57a885486daf9c68c",
+    "url": "/h/static/js/2.4ea57a88.chunk.js"
+  },
+  {
+    "revision": "a1880fbfb5634577b88090c9f82b3de7",
+    "url": "/h/static/media/platziconf-logo.a1880fbf.svg"
+  },
+  {
+    "revision": "ed7c58eafe17eff3b2557c0369b20264",
+    "url": "/h/static/media/badge-header.ed7c58ea.svg"
+  },
+  {
+    "revision": "6841730f1bbe3235e7a0cdc7d244d244",
+    "url": "/h/static/media/logo.6841730f.svg"
+  },
+  {
+    "revision": "0d2ff286deb3895d3ee82fed78c4dce4",
+    "url": "/h/static/media/astronauts.0d2ff286.svg"
+  },
+  {
+    "revision": "0abcdfcd1e323649a0338a204fe8b853",
+    "url": "/h/static/media/stars.0abcdfcd.svg"
+  },
+  {
+    "revision": "db99f935db40f737123192abd8b41a97",
+    "url": "/h/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/EdisonAlzate/h/precache-manifest.157137de2f2fd1eb9a8ebe018366b38b.js"
+  "/h/precache-manifest.188fc869135f97656023f0568e12486d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/EdisonAlzate/h/index.html", {
+workbox.routing.registerNavigationRoute("/h/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
